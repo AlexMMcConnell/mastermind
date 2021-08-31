92dcde0cd0b44ae332d78a368c90a5e646ff9af4
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-30 19:05:24 -0600

    Add - Create initial welcome and generation of game

--- mastermind.rb
+++ mastermind.rb
@@ -1,1 +1,13 @@
 require './lib/code_generator'
+require './lib/instructions'
+require './lib/game'
+
+print "Welcome to MASTERMIND
+
+"
+puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+
+answer = ""
+generator = CodeGenerator.new
+mastercode = generator.creator
+newgame = Game.new(mastercode)
