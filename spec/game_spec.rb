34b7e2f4b54248488187b2aa3662ebfd8ec44259
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-30 19:03:43 -0600

    Test - Fixed initialize test, add store guesses method test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,10 +4,10 @@ require './lib/game'
 describe 'Game' do
   describe '#initialize' do
     it 'stores generated code at @code' do
-      code_new = "RRBB"
+      code_new = "rrbb"
       master_code = Game.new(code_new)
 
-      expect(master_code.code).to eq("RRBB")
+      expect(master_code.code).to eq("rrbb")
     end
 
     it 'creates an empty array at @guesses' do
@@ -16,5 +16,22 @@ describe 'Game' do
 
       expect(master_code.guesses).to eq([])
     end
+  end
+
+  describe '#store_guesses' do
+    it 'stores guesses' do
+      code_new = "rrbb"
+      master_code = Game.new(code_new)
+      master_code.store_guesses("guess1")
+      master_code.store_guesses("guess2")
+
+      expect(master_code.guesses).to eq(["guess1", "guess2"])
+    end
+  end
+
+  describe '#guesser' do
+  end
+
+  describe '#answer' do
   end
 end
