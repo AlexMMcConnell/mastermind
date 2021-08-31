565c949555116a0f33637f982d5f3e3191374378
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-30 19:06:06 -0600

    Add - Add until loop for user input

--- mastermind.rb
+++ mastermind.rb
@@ -11,3 +11,21 @@ answer = ""
 generator = CodeGenerator.new
 mastercode = generator.creator
 newgame = Game.new(mastercode)
+
+until answer == "q"
+  print "> "
+  answer = gets.chomp.to_s
+  if answer == "i"
+    instructions
+    print '
+
+'
+    p "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+  elsif answer == "p"
+    print "I have generated a beginner sequence with four elements made up of: (r)ed,
+(g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
+
+"
+    newgame.guesser
+  end
+end
