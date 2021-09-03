50936135163761173326c77640a48db730b31d54
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-03 12:05:11 -0600

    Feat - Add difficulty selection to runner file

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,7 @@
 require './lib/code_generator'
 require './lib/instructions'
 require './lib/game'
+require 'pry'
 
 print "Welcome to MASTERMIND
 
@@ -8,25 +9,38 @@ print "Welcome to MASTERMIND
 puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
 
 answer = ""
-generator = CodeGenerator.new
 text = Text.new
+difficulty = "invalid"
 
-until answer == "q"
+until answer == "q" || answer == "quit"
   print "> "
   answer = gets.chomp.to_s
-  if answer == "i"
+  if answer == "i" || answer == "instructions"
     text.instructions
     print '
 
 '
     p "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-  elsif answer == "p"
-    print "I have generated a beginner sequence with four elements made up of: (r)ed,
-(g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
+  elsif answer == "p" || answer == "play"
+    p "What difficulty would you like to play on: (b)eginner, (i)ntermediate, or (a)dvanced?"
 
-"
-    mastercode = generator.creator
-    newgame = Game.new(mastercode)
-    newgame.game_runner
+      until difficulty != "invalid"
+        difficulty = gets.chomp.to_s
+        if difficulty == "beginner" || difficulty == "b"
+          text.beginner_sequence
+        elsif difficulty == "intermediate" || difficulty == "i"
+          text.intermediate_sequence
+        elsif difficulty == "advanced" || difficulty == "a"
+          text.advanced_sequence
+        elsif
+          p "Invalid input"
+          difficulty = "invalid"
+        end
+      end
+
+      generator = CodeGenerator.new(difficulty)
+      mastercode = generator.creator
+      newgame = Game.new(mastercode)
+      newgame.game_runner
   end
 end
