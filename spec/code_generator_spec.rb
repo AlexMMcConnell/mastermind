b5db3c06772cee38d9a6898af09b31e519bea493
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-03 12:19:54 -0600

    Test - Add more shuffler tests, add creator tests

--- spec/code_generator_spec.rb
+++ spec/code_generator_spec.rb
@@ -3,27 +3,53 @@ require './lib/code_generator'
 
 describe 'CodeGenerator' do
   describe '#shuffler' do
-    it 'creates an array of shuffled colors in letter format' do
-      generate = CodeGenerator.new
-      generate.shuffler
+    it 'generates an array' do
+      generate = CodeGenerator.new("beginner")
 
       expect(generate.shuffler).to be_a(Array)
+    end
+
+    it 'generates a 16 long array to randomize from if beginner' do
+      generate = CodeGenerator.new("beginner")
+
+      expect(generate.shuffler.length).to eq(16)
+    end
+
+    it 'generates a 30 long array to randomize from if intermediate' do
+      generate = CodeGenerator.new("intermediate")
+
+      expect(generate.shuffler.length).to eq(30)
+    end
+
+    it 'generates a 48 long array to randomize from if advanced' do
+      generate = CodeGenerator.new("advanced")
+
+      expect(generate.shuffler.length).to eq(48)
     end
   end
 
   describe '#creator' do
     it 'is a string' do
-      generate = CodeGenerator.new
+      generate = CodeGenerator.new("intermediate")
       generate.creator
 
       expect(generate.creator).to be_a(String)
     end
 
     it 'is four characters long for beginner difficulty' do
-      generate = CodeGenerator.new
-      generate.creator
+      generate = CodeGenerator.new("beginner")
 
-      expect(generate.creator.length).to be(4)
+      expect(generate.code_length).to eq(4)
+    end
+    it 'is six characters long for intermediate difficulty' do
+      generate = CodeGenerator.new("intermediate")
+
+      expect(generate.code_length).to eq(6)
+    end
+    it 'is eight characters long for advanced difficulty' do
+      generate = CodeGenerator.new("advanced")
+
+      expect(generate.code_length).to eq(8)
     end
   end
 end
