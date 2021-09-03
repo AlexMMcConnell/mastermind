37c4608c8017391dc8f7f0cd28deb6838ee93593
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-03 12:04:25 -0600

    Feat - Add difficulty selection and code generation

--- lib/code_generator.rb
+++ lib/code_generator.rb
@@ -1,13 +1,32 @@
 class CodeGenerator
+  attr_reader :difficulty
+
+  def initialize(difficulty)
+    @difficulty = difficulty
+  end
+
   def shuffler
-    #eventually create an if/else branch for intermediate/expert difficulty
-    beginner_init_code = "rrrrbbbbggggyyyy"
-    beginner_init_code.split("").shuffle
+    if difficulty == "beginner" || difficulty == "b"
+      beginner_init_code = "rrrrbbbbggggyyyy"
+      beginner_init_code.split("").shuffle
+    elsif difficulty == "intermediate" || difficulty == "i"
+      intermediate_init_code = "rrrrrrbbbbbbggggggyyyyyyoooooo"
+      intermediate_init_code.split("").shuffle
+    elsif difficulty == "advanced" || difficulty == "a"
+      advanced_init_code = "rrrrrrrrbbbbbbbbggggggggyyyyyyyyoooooooopppppppp"
+      advanced_init_code.split("").shuffle
+    end
   end
 
   def creator
     shuffled_code = shuffler
-    mastercode = shuffled_code[0..3]
+    if difficulty == "beginner" || difficulty == "b"
+      mastercode = shuffled_code[0..3]
+    elsif difficulty == "intermediate" || difficulty == "i"
+      mastercode = shuffled_code[0..5]
+    else difficulty == "advanced" || difficulty == "a"
+      mastercode = shuffled_code[0..7]
+    end
     mastercode.flatten.join
   end
 end
