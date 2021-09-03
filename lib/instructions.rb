12fffdc92d40d5c1093eb2d7fb27d1c8e839d9dd
Micha Bernhard <mib70@pitt.edu>
2021-09-02 19:21:33 -0600

    Feat - Add Text class

--- lib/instructions.rb
+++ lib/instructions.rb
@@ -1,18 +1,20 @@
-def instructions # outputs instructions for playing Mastermind into terminal
-  print "Instructions for Mastermind:
+class Text
+
+  def instructions # outputs instructions for playing Mastermind into terminal
+    print "Instructions for Mastermind:
 
 "
-  print "Mastermind is a game where you attempt to guess the code that the Mastermind
-or keyholder, has given. You have to enter colors that match up with the code
-that the keyholder has determined using single letters for each color:
+    print "Mastermind is a game where you attempt to guess the code that the Mastermind
+  or keyholder, has given. You have to enter colors that match up with the code
+  that the keyholder has determined using single letters for each color:
 
 "
   # if branch for game types later
-  print " r for red, g for green, b for blue, and y for yellow
+    print " r for red, g for green, b for blue, and y for yellow
 
 "
-  print "You guess in a four letter format (rgby, for instance), and the game will
-give you a response detailing how many of your guesses were of the correct
-color, and which were in the correct position. Once you have guessed the
-full code correctly, you will have won the game!"
+    print "You guess in a four letter format (rgby, for instance), and the game will
+  give you a response detailing how many of your guesses were of the correct
+  color, and which were in the correct position. Once you have guessed the
+  full code correctly, you will have won the game!"
 end
