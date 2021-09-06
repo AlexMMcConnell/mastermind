ab9430b309e8d40081713aa3920a933288f426e7
Micha Bernhard <mib70@pitt.edu>
2021-09-06 13:12:23 -0600

    Refactor - Refactor code into mm_helper

--- mastermind.rb
+++ mastermind.rb
@@ -1,53 +1,10 @@
 require './lib/code_generator'
 require './lib/instructions'
 require './lib/game'
+require './lib/mm_helper'
 require 'pry'
 
-print "Welcome to MASTERMIND
-
-"
-
-
-answer = ""
 text = Text.new
-difficulty = "invalid"
-puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-print "> "
-answer = gets.chomp.to_s
+helper = Helper.new(text)
 
-until answer == "q" || answer == "quit" || answer == "yes"
-  if answer == "i" || answer == "instructions"
-    text.instructions
-    print '
-
-'
-    p "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-  elsif answer == "p" || answer == "play"
-    p "What difficulty would you like to play on: (b)eginner, (i)ntermediate, or (a)dvanced?"
-
-      until difficulty != "invalid"
-        difficulty = gets.chomp.to_s
-        if difficulty == "beginner" || difficulty == "b"
-          text.beginner_sequence
-        elsif difficulty == "intermediate" || difficulty == "i"
-          text.intermediate_sequence
-        elsif difficulty == "advanced" || difficulty == "a"
-          text.advanced_sequence
-        elsif
-          p "Invalid input"
-          difficulty = "invalid"
-        end
-      end
-
-      generator = CodeGenerator.new(difficulty)
-      mastercode = generator.creator
-      newgame = Game.new(mastercode)
-      newgame.game_runner
-      difficulty = "invalid"
-  end
-  answer = gets.chomp.to_s
-  if answer != "q" || answer != "quit" || answer != "yes"
-    puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-    print "> "
-  end
-end
+helper.game_start
