e04429e918b2fe565ad059f8a413c9071915c7cf
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-07 17:41:04 -0600

    Fix - Adjust file requirements for mastermind.rb

--- mastermind.rb
+++ mastermind.rb
@@ -1,10 +1,10 @@
 require './lib/code_generator'
-require './lib/instructions'
+require './lib/mastermind_text'
 require './lib/game'
 require './lib/mm_helper'
 require 'pry'
 
-text = Text.new
-helper = Helper.new(text)
+text = MastermindText.new
+helper = MastermindHelper.new(text)
 
 helper.game_start
